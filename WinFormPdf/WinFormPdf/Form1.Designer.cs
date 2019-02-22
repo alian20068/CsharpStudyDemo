b2df166c5e4dd4ef69ace4eeb4ed5dffb0c0dc33
alian20068 <newjoin@foxmail.com>
2019-02-22 23:03:05 +0800

    pdf读取

---
+++ WinFormPdf/WinFormPdf/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WinFormPdf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.lblFullName = new System.Windows.Forms.Label();
+            this.rtxShow = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(12, 12);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "选择文件";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(93, 17);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(0, 12);
+            this.lblFileName.TabIndex = 2;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(12, 69);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 3;
+            this.btnRead.Text = "读取";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // lblFullName
+            // 
+            this.lblFullName.AutoSize = true;
+            this.lblFullName.Location = new System.Drawing.Point(10, 45);
+            this.lblFullName.Name = "lblFullName";
+            this.lblFullName.Size = new System.Drawing.Size(0, 12);
+            this.lblFullName.TabIndex = 4;
+            // 
+            // rtxShow
+            // 
+            this.rtxShow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtxShow.Location = new System.Drawing.Point(12, 98);
+            this.rtxShow.Name = "rtxShow";
+            this.rtxShow.Size = new System.Drawing.Size(576, 250);
+            this.rtxShow.TabIndex = 5;
+            this.rtxShow.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 360);
+            this.Controls.Add(this.rtxShow);
+            this.Controls.Add(this.lblFullName);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.btnSelect);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "PDF文件读取";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Label lblFullName;
+        private System.Windows.Forms.RichTextBox rtxShow;
+    }
+}
+
---
+++ WinFormPdf/WinFormPdf/Form1.cs
@@ -0,0 +1,68 @@
+﻿using org.pdfbox.pdmodel;
+using org.pdfbox.util;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormPdf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog selectFileDialog = new OpenFileDialog();
+            //selectFileDialog.InitialDirectory = "C:\\"; //初始目录
+            selectFileDialog.Filter = "便携式文档|*.pdf|所有文件|*.*"; //文件过滤
+            selectFileDialog.FilterIndex = 1; //默认第一个过滤条件
+            selectFileDialog.RestoreDirectory = true; //保存上一次目录
+            selectFileDialog.Title = "请选择文件";
+            //selectFileDialog.Multiselect = true;//设置多选
+
+            if (selectFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string strExtension = Path.GetExtension(selectFileDialog.FileName).ToLower();
+                string[] arr = new string[] { ".pdf" };
+                if (!((IList)arr).Contains(strExtension))
+                {
+                    MessageBox.Show("仅能上传pdf格式的文件！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    lblFileName.Text = "文件名：" + selectFileDialog.SafeFileName;
+                    lblFullName.Text = "完整路径：" + selectFileDialog.FileName;
+                }
+            }
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(lblFullName.Text))
+            {
+                MessageBox.Show("请先选择文件！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            
+            //参考 https://www.cnblogs.com/yjd_hycf_space/p/7942444.html
+
+            PDDocument doc = PDDocument.load(lblFullName.Text.Replace("完整路径：", ""));
+            PDFTextStripper pdfStripper = new PDFTextStripper();
+            string text = pdfStripper.getText(doc);
+            //StreamWriter sw = new StreamWriter("", false, Encoding.GetEncoding("gb2312"));
+            //sw.Write(text);
+            //sw.Close();
+            rtxShow.Text = text;
+
+        }
+    }
+}
