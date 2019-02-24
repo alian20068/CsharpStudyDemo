96c52da55a0de6b98b3f27597279096a460b758b
alian20068 <newjoin@foxmail.com>
2019-02-24 21:35:10 +0800

    提交

--- WinFormPdf/WinFormPdf/Form1.Designer.cs
+++ WinFormPdf/WinFormPdf/Form1.Designer.cs
@@ -33,13 +33,22 @@
             this.btnRead = new System.Windows.Forms.Button();
             this.lblFullName = new System.Windows.Forms.Label();
             this.rtxShow = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBegin = new System.Windows.Forms.TextBox();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.lblCnt = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.txtSaveFolder = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnSelect
             // 
-            this.btnSelect.Location = new System.Drawing.Point(12, 12);
+            this.btnSelect.Location = new System.Drawing.Point(16, 15);
+            this.btnSelect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSelect.Name = "btnSelect";
-            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.Size = new System.Drawing.Size(100, 29);
             this.btnSelect.TabIndex = 1;
             this.btnSelect.Text = "选择文件";
             this.btnSelect.UseVisualStyleBackColor = true;
@@ -48,17 +57,19 @@
             // lblFileName
             // 
             this.lblFileName.AutoSize = true;
-            this.lblFileName.Location = new System.Drawing.Point(93, 17);
+            this.lblFileName.Location = new System.Drawing.Point(124, 21);
+            this.lblFileName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblFileName.Name = "lblFileName";
-            this.lblFileName.Size = new System.Drawing.Size(0, 12);
+            this.lblFileName.Size = new System.Drawing.Size(0, 15);
             this.lblFileName.TabIndex = 2;
             // 
             // btnRead
             // 
-            this.btnRead.Location = new System.Drawing.Point(12, 69);
+            this.btnRead.Location = new System.Drawing.Point(16, 85);
+            this.btnRead.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnRead.Name = "btnRead";
-            this.btnRead.Size = new System.Drawing.Size(75, 23);
-            this.btnRead.TabIndex = 3;
+            this.btnRead.Size = new System.Drawing.Size(100, 29);
+            this.btnRead.TabIndex = 4;
             this.btnRead.Text = "读取";
             this.btnRead.UseVisualStyleBackColor = true;
             this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
@@ -66,35 +77,120 @@
             // lblFullName
             // 
             this.lblFullName.AutoSize = true;
-            this.lblFullName.Location = new System.Drawing.Point(10, 45);
+            this.lblFullName.Location = new System.Drawing.Point(13, 56);
+            this.lblFullName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblFullName.Name = "lblFullName";
-            this.lblFullName.Size = new System.Drawing.Size(0, 12);
-            this.lblFullName.TabIndex = 4;
+            this.lblFullName.Size = new System.Drawing.Size(0, 15);
+            this.lblFullName.TabIndex = 3;
             // 
             // rtxShow
             // 
             this.rtxShow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.rtxShow.Location = new System.Drawing.Point(12, 98);
+            this.rtxShow.Location = new System.Drawing.Point(16, 122);
+            this.rtxShow.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rtxShow.Name = "rtxShow";
-            this.rtxShow.Size = new System.Drawing.Size(576, 250);
-            this.rtxShow.TabIndex = 5;
+            this.rtxShow.Size = new System.Drawing.Size(767, 312);
+            this.rtxShow.TabIndex = 10;
             this.rtxShow.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(238, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "-----";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(389, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "页";
+            // 
+            // txtBegin
+            // 
+            this.txtBegin.Location = new System.Drawing.Point(136, 89);
+            this.txtBegin.Name = "txtBegin";
+            this.txtBegin.Size = new System.Drawing.Size(100, 25);
+            this.txtBegin.TabIndex = 5;
+            this.txtBegin.Text = "1";
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(287, 89);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(100, 25);
+            this.txtEnd.TabIndex = 7;
+            this.txtEnd.Text = "1";
+            // 
+            // lblCnt
+            // 
+            this.lblCnt.AutoSize = true;
+            this.lblCnt.Location = new System.Drawing.Point(453, 92);
+            this.lblCnt.Name = "lblCnt";
+            this.lblCnt.Size = new System.Drawing.Size(0, 15);
+            this.lblCnt.TabIndex = 9;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(577, 441);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 29);
+            this.btnSave.TabIndex = 12;
+            this.btnSave.Text = "保存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNext.Location = new System.Drawing.Point(683, 441);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(100, 29);
+            this.btnNext.TabIndex = 13;
+            this.btnNext.Text = "下一页";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // txtSaveFolder
+            // 
+            this.txtSaveFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSaveFolder.Location = new System.Drawing.Point(16, 445);
+            this.txtSaveFolder.Name = "txtSaveFolder";
+            this.txtSaveFolder.Size = new System.Drawing.Size(553, 25);
+            this.txtSaveFolder.TabIndex = 11;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 360);
+            this.ClientSize = new System.Drawing.Size(800, 483);
+            this.Controls.Add(this.txtSaveFolder);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblCnt);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.txtBegin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.rtxShow);
             this.Controls.Add(this.lblFullName);
             this.Controls.Add(this.btnRead);
             this.Controls.Add(this.lblFileName);
             this.Controls.Add(this.btnSelect);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "PDF文件读取";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,6 +202,14 @@
         private System.Windows.Forms.Button btnRead;
         private System.Windows.Forms.Label lblFullName;
         private System.Windows.Forms.RichTextBox rtxShow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBegin;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.Label lblCnt;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.TextBox txtSaveFolder;
     }
 }
 
