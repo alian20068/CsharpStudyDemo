d6a16511cce9ce8ad90867d9e5e278e95d48ae8c
alian20068 <newjoin@foxmail.com>
2019-06-12 22:51:57 +0800

    ElasticSearch6.4.2 NEST6.0.0

---
+++ ElasticSearch/Console642/Class/Setting.cs
@@ -0,0 +1,24 @@
+﻿using Nest;
+using System;
+
+namespace Console642.Class
+{
+    class Setting
+    {
+        public static string strConnectionString = @"http://localhost:9200";
+        public static Uri Node
+        {
+            get
+            {
+                return new Uri(strConnectionString);
+            }
+        }
+        public static ConnectionSettings ConnectionSettings
+        {
+            get
+            {
+                return new ConnectionSettings(Node).DefaultIndex("default");
+            }
+        }
+    }
+}
---
+++ ElasticSearch/Console642/Program.cs
@@ -0,0 +1,39 @@
+﻿using Console642.Class;
+using Nest;
+using System;
+using System.Linq;
+
+namespace Console642
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var node = new Uri("http://localhost:9200");
+            var settings = new ConnectionSettings(node);
+            var client = new ElasticClient(settings);
+
+            //elasticsearch-6.4.2.msi
+            //NEST 6.0.0
+
+            //创建索引
+            ESProvider es = new ESProvider();
+            es.PopulateIndex(new MeetupEvents
+            {
+                eventid = 2,
+                eventname = "eventname2",
+                orignalid = "orignalid2",
+                description = "description1"
+            });
+
+            SearchRequest sr = new SearchRequest("meetup", "events");
+            TermQuery tq = new TermQuery();
+            tq.Field = "description";
+            tq.Value = "description1";
+            sr.Query = tq;
+            var result = client.Search<MeetupEvents>(sr);
+            var result2 = result.Documents.ToList<MeetupEvents>();
+
+        }
+    }
+}
