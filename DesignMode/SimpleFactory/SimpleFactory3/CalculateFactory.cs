443038ff5b47ff0a7c4dc7b6ce4051ee6a9f7075
alian20068 <newjoin@foxmail.com>
2019-06-16 21:23:26 +0800

    简单工厂

---
+++ DesignMode/SimpleFactory/SimpleFactory3/CalculateFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleFactory3
+{
+    class CalculateFactory
+    {
+        public static CalculateBase CreateCalculate(string operate)
+        {
+            CalculateBase calculate = null;
+
+            switch (operate)
+            {
+                case "+":
+                    calculate = new CalculateAdd();
+                    break;
+                case "-":
+                    calculate = new CalculateSub();
+                    break;
+                case "*":
+                    calculate = new CalculateMul();
+                    break;
+                case "/":
+                    calculate = new CalculateDiv();
+                    break;
+            }
+
+            return calculate;
+        }
+    }
+}
--- DesignMode/SimpleFactory/SimpleFactory3/Program.cs
+++ DesignMode/SimpleFactory/SimpleFactory3/Program.cs
@@ -6,7 +6,30 @@ namespace SimpleFactory3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("简单工厂");
+            Console.WriteLine(@"到底要实例化哪个运算对象，将来会不会增加实例化对象（比如开根运算），
+这是很容易变化地方，应该考虑用一个单独类来做创建实例的过程");
+            Console.WriteLine("");
+
+            Console.Write("请输入数字A：");
+            string numberA = Console.ReadLine();
+            Console.Write("请选择运算符号（+、-、*、/）：");
+            string strOperate = Console.ReadLine();
+            Console.Write("请输入数字B：");
+            string numberB = Console.ReadLine();
+
+            double.TryParse(numberA, out var dA);
+            double.TryParse(numberB, out var dB);
+
+            CalculateBase calculate;
+            calculate = CalculateFactory.CreateCalculate(strOperate);
+            calculate.NumberA = dA;
+            calculate.NumberB = dB;
+
+            string strResult = calculate.GetResult().ToString();
+
+            Console.WriteLine("结果是：" + strResult);
+            Console.ReadLine();
         }
     }
 }
