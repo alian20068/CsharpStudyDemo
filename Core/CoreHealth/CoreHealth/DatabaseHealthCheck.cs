c7e27c9d1daed5ae666c7c01372354fad3431db4
NewJoin <NewJoin@live.cn>
2019-11-17 22:15:34 +0800

    健康检查

---
+++ Core/CoreHealth/CoreHealth/DatabaseHealthCheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace CoreHealth
+{
+    /// <summary>
+    /// 数据库健康检查
+    /// </summary>
+    public class DatabaseHealthCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            using (var conn = new System.Data.SqlClient.SqlConnection(""))
+            {
+                try
+                {
+                    conn.Open();
+                }
+                catch (System.Data.SqlClient.SqlException)
+                {
+                    return Task.FromResult(HealthCheckResult.Unhealthy());
+                }
+            }
+            return Task.FromResult(HealthCheckResult.Healthy());
+        }
+    }
+}
---
+++ Core/CoreHealth/CoreHealth/Startup.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace CoreHealth
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            //健康检查
+            services.AddHealthChecks()
+                //可添加自定义检查
+                .AddCheck("sql", () =>
+                {
+                    using (var conn = new System.Data.SqlClient.SqlConnection(""))
+                    {
+                        try
+                        {
+                            conn.Open();
+                        }
+                        catch (System.Data.SqlClient.SqlException)
+                        {
+                            return HealthCheckResult.Unhealthy();
+                        }
+                    }
+                    return HealthCheckResult.Healthy();
+                });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+
+            //健康检查
+            app.UseHealthChecks("/health");
+        }
+    }
+}
