5d1d16477e18955148b9ae699a6b37b77fcb7de6
newjoin <newjoin@foxmail.com>
2019-11-20 18:44:40 +0800

    Lock锁

---
+++ Core/LockDemo/LockDemo/Model/Account.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LockDemo.Model
+{
+    public class Account
+    {
+        private readonly object lockObj = new object();
+        private decimal balance;
+
+        public Account(decimal initialBalance)
+        {
+            balance = initialBalance;
+        }
+
+        public decimal Debit(decimal amount)
+        {
+            lock (lockObj)
+            {
+                if (balance >= amount)
+                {
+                    Console.WriteLine($"借前:{balance,5}");
+                    Console.WriteLine($"借走:{amount,5}");
+                    balance = balance - amount;
+                    Console.WriteLine($"借后:{balance,5}");
+                    
+                    return amount;
+                }
+                else
+                    return 0;
+            }
+        }
+
+        public void Credit(decimal amount)
+        {
+            lock (lockObj)
+            {
+                Console.WriteLine($"贷前:{balance,5}");
+                Console.WriteLine($"贷来:{amount,5}");
+                balance = balance + amount;
+                Console.WriteLine($"贷后:{balance,5}");
+            }
+        }
+
+    }
+}
---
+++ Core/LockDemo/LockDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LockDemo
+{
+    using Model;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var account = new Account(1000);
+            var arrTask = new Task[5];
+
+            for (int i = 0; i < arrTask.Length; i++)
+            {
+                arrTask[i] = Task.Run(
+                    () => RandomlyUpdate(account)
+                    );
+            }
+
+            Task.WaitAll(arrTask);
+        }
+
+        static void RandomlyUpdate(Account account)
+        {
+            var rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                var amount = rnd.Next(1, 100);
+                var doCredit = rnd.NextDouble() < 0.5;
+                if (doCredit)
+                    account.Credit(amount);
+                else
+                    account.Debit(amount);
+            }
+        }
+
+    }
+}
